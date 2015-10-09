9f39a1c43e3c1769b0320aabf98afe67c4426432
YihanWang2015 <ywang4241@mail.kvcc.edu>
2015-10-09 15:31:41 -0400

    Add TemperatureType Class

--- app/src/main/java/edu/kvcc/cis298/cis298assignment2/TemperatureConverter.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment2/TemperatureConverter.java
@@ -4,14 +4,162 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class TemperatureConverter extends AppCompatActivity {
+
+
+    private RadioGroup mFromRadioGroup;
+    private RadioGroup mToRadioGroup;
+    private RadioButton mType1;
+    private RadioButton mType2;
+    private RadioButton mType3;
+    private RadioButton mType4;
+    private RadioButton mType5;
+    private RadioButton mType6;
+    private RadioButton mType7;
+    private RadioButton mType8;
+
+    private Button mCovertButton;
+
+    private TextView mResultTextView;
+    private TextView mFormulaTextView;
+
+    private EditText userInputText;
+
+
+    private RadioButton fromRadioButton;
+
+
+/*    public void onRadioButtonClicked(View view){
+
+        boolean checked = ((RadioButton)view).isChecked();
+
+        switch(view.getId()){
+            case R.id.type_1:
+                if(checked){
+                    mResultTextView.setText("C");
+                }
+                break;
+            case R.id.type_2:
+                if(checked){
+                    mResultTextView.setText("F");
+                }
+                break;
+            case R.id.type_3:
+                if(checked){
+                    mResultTextView.setText("K");
+                }
+                break;
+            case R.id.type_4:
+                if(checked){
+                    mResultTextView.setText("R");
+                }
+                break;
+        }
+
+    }*/
+
+
+/*    public void test(){
+        if(mType1.isChecked()){
+            mResultTextView.setText("C");
+
+        }else if (mType2.isChecked()){
+            mResultTextView.setText("F");
+
+        }else if(mType3.isChecked()){
+            mResultTextView.setText("K");
+
+        }else if(mType4.isChecked()){
+           mResultTextView.setText("R");
+        }
+
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature_converter);
-    }
+
+        mResultTextView =(TextView)findViewById(R.id.result_text);
+        userInputText = (EditText) findViewById(R.id.editText);
+
+        mFromRadioGroup = (RadioGroup) findViewById(R.id.from_group);
+        mToRadioGroup = (RadioGroup) findViewById(R.id.to_group);
+
+        mType1 = (RadioButton) findViewById(R.id.type_1);
+        mType2 = (RadioButton) findViewById(R.id.type_2);
+        mType3 = (RadioButton) findViewById(R.id.type_3);
+        mType4 = (RadioButton) findViewById(R.id.type_4);
+        mType5 = (RadioButton) findViewById(R.id.type_5);
+        mType6 = (RadioButton) findViewById(R.id.type_6);
+        mType7 = (RadioButton) findViewById(R.id.type_7);
+        mType8 = (RadioButton) findViewById(R.id.type_8);
+
+        mCovertButton = (Button) findViewById(R.id.convert_button);
+        mCovertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (userInputText.getText().length() == 0) {
+
+                    Toast.makeText(TemperatureConverter.this,
+                            R.string.reminder_toast,
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+          /*      int selectedId = mFromRadioGroup.getCheckedRadioButtonId();
+                fromRadioButton = (RadioButton)findViewById(selectedId);
+                Toast.makeText(TemperatureConverter.this,
+                                fromRadioButton.getText(),
+                                Toast.LENGTH_SHORT).show();
+                mResultTextView.setText(fromRadioButton.getText());*/
+
+
+                 double inputValue = Double.parseDouble(userInputText.getText().toString());
+
+                if(mType1.isChecked())
+                {
+                    if(mType7.isChecked()){
+                        mResultTextView.setText(String.valueOf(TemperatureType.CelsiusToKelvin(inputValue)));
+                    }
+
+                    if(mType8.isChecked()){
+                         mResultTextView.setText(String.valueOf(TemperatureType.CelsiusToRankin(inputValue)));
+                    }
+
+                }
+
+
+            }
+
+
+        });
+
+
+
+
+
+
+
+
+
+    }//onCreate
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment2/TemperatureType.java
@@ -0,0 +1,80 @@
+package edu.kvcc.cis298.cis298assignment2;
+
+/**
+ * Created by bigy on 10/9/15.
+ */
+public class TemperatureType {
+
+    //Formula from http://www.csgnetwork.com/temp2conv.html
+
+    //convert from celsius to to other types:
+   public static double CelsiusToCelsius(double celsius){
+       return celsius;
+   }
+
+
+    public static double CelsiusToFahrenheit(double celsius){
+        return celsius * 1.8 + 32;
+    }
+
+    public static double CelsiusToKelvin(double celsius){
+        return celsius + 237.15;
+    }
+
+    public static double CelsiusToRankin(double celsius){
+        return celsius * 1.8 + 32 + 459.67;
+    }
+
+    //convert from fahrenheit to other types:
+    public static double FahrenheitToFahrenheit(double fahrenheit){
+        return fahrenheit;
+    }
+
+    public static double FahrenheitToCelsius(double fahrenheit){
+        return (fahrenheit - 32) / 1.8;
+    }
+
+    public static double FahrenheitToKelvin(double fahrenheit){
+        return (fahrenheit + 459.67) / 1.8;
+    }
+
+    public static double FahrenheitToRankin(double fahrenheit){
+        return fahrenheit + 459.67;
+    }
+
+    //convert from kelvin to other types:
+    public static double KelvinToKelvin(double kelvin){
+        return kelvin;
+    }
+    public static double KelvinToCelsius(double kelvin){
+        return kelvin-273.15;
+    }
+
+    public static double KelvinToFahrenheit(double kelvin){
+        return kelvin * 1.8 - 459.67;
+    }
+
+    public static double KelvinToRankin(double kelvin){
+        return kelvin * 1.8;
+    }
+
+    //convert from rankin to other types:
+
+    public static double RankinToRankin(double rankin){
+        return rankin;
+    }
+    public static double RankinToCelsius(double rankin){
+        return  ( rankin - 32 - 459.67) / 1.8;
+    }
+
+    public static double RankinToFahrenheit(double rankin){
+        return rankin - 459.67;
+    }
+
+    public static double RankinToKelvin(double rankin){
+        return rankin * 1.8;
+    }
+
+}
+
+
